65070e024ac4e9f8616e98c89f2baceb5f0f2628
Joel <joelmik123@gmail.com>
2021-10-30 19:03:13 +0200

    added contact form styling

--- pages/index.js
+++ pages/index.js
@@ -57,21 +57,21 @@ export default function Home() {
         </section>
 
         {/* Services Section */}
-        <section className='services flex justify-center items-center flex-col h-[90vh] py-14 px-6 md:px-16 lg:px-32 lg:py-24 bg-[#101010]'>
+        <section className='services flex justify-center items-center flex-col lg:h-[90vh] py-14 px-6 md:px-16 lg:px-32 lg:py-24 bg-[#101010]'>
           <h1 data-aos="fade-up" className="text-center text-white text-4xl md:text-6xl font-bold">Onze geweldige diensten</h1>
           <p data-aos="fade-up" className="text-[#717173] text-lg md:text-xl font-light text-center pt-4 pb-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
-          <div className="flex gap-5 flex-wrap justify-center">
-            <div data-aos="fade-up" className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
+          <div data-aos="fade-up" data-aos-delay="150" className="flex gap-5 flex-wrap justify-center">
+            <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
               <ChartBarIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
               <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Online Branding</h2>
               <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
             </div>
-            <div data-aos="fade-up" data-aos-delay="150" className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
+            <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
               <CodeIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
               <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Website Development</h2>
               <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
             </div>
-            <div data-aos="fade-up" data-aos-delay="300" className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
+            <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
               <FilmIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
               <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Video & animatie</h2>
               <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
@@ -81,12 +81,12 @@ export default function Home() {
         </section>
   
         {/* Contact Section */}
-        <section className='contact flex justify-center py-14 md:px-32 lg:px-64 lg:py-24  bg-[#101010]'>
-          <div className="pr-5 w-1/2">
+        <section className='contact flex flex-col items-center lg:flex-row text-center lg:text-left justify-center py-14 md:px-32 lg:px-64 lg:py-24  bg-[#101010]'>
+          <div className="pr-5 w-10/12 lg:w-1/2">
             <h1 data-aos="fade-up" className="text-white text-4xl md:text-6xl font-bold">Laten we samen een project starten.</h1>
-            <p className="text-[#717173] font-light text-lg mt-5">I’d love to meet up with you to discuss your venture, and potential collaborations.</p>
+            <p className="text-[#717173] font-light text-lg my-5">I’d love to meet up with you to discuss your venture, and potential collaborations.</p>
           </div>
-          <div className="w-1/2">
+          <div className="w-10/12 lg:w-1/2">
             <ContactForm />
           </div>
         </section>
