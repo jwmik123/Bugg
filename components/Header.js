bd833dbad26ee54218adb439600f7b1339b4b351
Joel <joelmik123@gmail.com>
2021-10-30 17:12:30 +0200

    added services

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
-import { useRouter } from 'next/dist/client/router';
+import { useRouter } from 'next/dist/client/router'
 
-import { 
-    MenuIcon
- } from '@heroicons/react/outline';
+import animateScrollTo from 'animated-scroll-to'
+
+import { MenuIcon } from '@heroicons/react/outline';
 
 function Header() {
 
@@ -20,32 +20,14 @@ function Header() {
             </div>
             <div className='hidden md:flex items-center gap-20 text-base font-medium'>
                 <button>Home</button>
-                <button>Projecten</button>
+                <button onClick={() => {animateScrollTo(document.querySelector('.services'))}}>Diensten</button>
                 <button>Over Ons</button>
                 <button>Contact</button>
             </div>
             <div className='hidden lg:block'>
-                <button className='border-2 border-[#F5D905] text-[#F5D905] px-4 py-2 rounded'>Start een project</button>
+                <button className='border-2 border-[#F1D302] text-[#F1D302] px-4 py-2 rounded'>Start een project</button>
             </div>
         </header>
-        // <header className="sticky bg-transparent shadow-sm top-0 z-50 grid grid-cols-2 pt-5 pb-6 px-10 lg:px-20 transition transform ease-in duration-200">
-        //     <div className='relative flex items-center h-8 cursor-pointer my-auto' onClick={() => router.push('/')}>
-        //         <h1 className="text-5xl font-staat text-white my-auto">BUGG.</h1>
-        //     </div>
-        //     <div className='flex justify-between'>
-        //         <div className='flex justify-end my-auto lg:hidden'>
-        //             <MenuIcon className="w-10 h-10  cursor-pointer text-white "/>
-        //         </div>
-        //         <div className="flex justify-between">
-        //             <p className='menu-item' onClick={() => router.push('/projects')}>Projecten</p>
-        //             <p className='menu-item' onClick={() => router.push('/about')}>Over ons</p>
-        //             <p className='menu-item' onClick={() => router.push('/contact')}>Contact</p>
-        //         </div>
-        //         <div>
-        //             <button className='text-[#042825] bg-white rounded-lg px-4 py-2 active:scale-95 transition transform ease-out'>Start een project</button>   
-        //         </div>
-        //     </div>
-        // </header>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,12 @@ import Head from 'next/head'
 import ContactForm from '../components/ContactForm'
 import Header from '../components/Header'
 
+import animateScrollTo from 'animated-scroll-to'
+
 import Image from 'next/image'
-import landingImage from '../assets/images/landing-image.jpeg'
+import landingImage from '../assets/images/landing-image.webp'
+
+import { ChevronDownIcon, TemplateIcon } from '@heroicons/react/outline'
 
 export default function Home() {
 
@@ -19,9 +23,10 @@ export default function Home() {
       {/* Header */}
       <Header />
       <main>
+        {/* Landing Section */}
         <section className='relative h-[50vh] md:h-[100vh] flex items-center justify-center '>
           <Image
-            className='opacity-60'
+            className='opacity-30'
             src={landingImage}
             layout='fill'
             objectFit='cover'
@@ -30,15 +35,49 @@ export default function Home() {
           <div className='absolute flex flex-col gap-1 md:gap-3 font-bold md:left-32 lg:left-64'>
           <h1 className="text-white text-4xl md:text-7xl">Hi, welkom bij Bugg.</h1>
             <TextLoop 
-              className='text-[#F5D905] text-2xl md:text-6xl' 
+              className='text-[#F1D302] text-2xl md:text-6xl' 
               springConfig={{ stiffness: 60, damping: 11 }} 
               children={["Website Development", "Online Branding", "Video's & Animaties"]} 
             />
             <h3 className='text-white text-1xl md:text-4xl'>In Amsterdam</h3>
+            <ChevronDownIcon 
+              className="hidden lg:block h-12 w-12 text-white mt-12 animate-bounce cursor-pointer" 
+              onClick={() => {animateScrollTo(document.querySelector('.services'))}} />
           </div>
         </section>
-        <section>
-          <ContactForm />
+
+        {/* Services Section */}
+        <section className='services py-14 px-16 md:px-16 lg:px-32 lg:py-24 bg-[#101010]'>
+          <h1 className="text-center text-white text-4xl md:text-6xl font-bold">Onze geweldige diensten</h1>
+          <p className="text-[#717173] text-lg md:text-xl font-light text-center pt-4 pb-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+          <div className="flex gap-5 flex-wrap justify-center">
+            <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
+              <TemplateIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
+              <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Online Branding</h2>
+              <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
+            </div>
+            <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
+              <TemplateIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
+              <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Website Development</h2>
+              <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
+            </div>
+            <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
+              <TemplateIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
+              <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Video & animatie</h2>
+              <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
+            </div>
+      
+          </div>
+        </section>
+        {/* hover:bg-gradient-to-r from-[#C1292E] to-[#F1D302]  */}
+        {/* Contact Section */}
+        <section className='contact flex py-14 md:px-32 lg:px-64 lg:py-24  bg-[#101010]'>
+          <div className="pr-5 w-1/2">
+            <h1 className="text-white text-4xl md:text-6xl font-bold">Laten we samen een project starten.</h1>
+          </div>
+          <div>
+            <ContactForm />
+          </div>
         </section>
       </main>
     </div>
