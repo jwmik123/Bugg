de74248a1d8966986b97a0f7d396b46b18cdf34d
Joel <joelmik123@gmail.com>
2021-10-30 17:24:14 +0200

    Updated service icons

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import animateScrollTo from 'animated-scroll-to'
 import Image from 'next/image'
 import landingImage from '../assets/images/landing-image.webp'
 
-import { ChevronDownIcon, TemplateIcon } from '@heroicons/react/outline'
+import { ChevronDownIcon, CodeIcon, ChartBarIcon, FilmIcon } from '@heroicons/react/outline'
 
 export default function Home() {
 
@@ -52,17 +52,17 @@ export default function Home() {
           <p className="text-[#717173] text-lg md:text-xl font-light text-center pt-4 pb-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
           <div className="flex gap-5 flex-wrap justify-center">
             <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
-              <TemplateIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
+              <ChartBarIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
               <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Online Branding</h2>
               <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
             </div>
             <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
-              <TemplateIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
+              <CodeIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
               <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Website Development</h2>
               <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
             </div>
             <div className="group bg-[#1d1d24] hover:bg-[#F1D302] min-w-[300px] w-10/12 md:w-3/6 lg:w-3/12 p-14 rounded-lg flex flex-col hover:cursor-pointer transform-gpu md:hover:translate-y-3 transition-all duration-500">
-              <TemplateIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
+              <FilmIcon className="w-14 h-14 mb-5 text-[#F1D302] group-hover:text-[#1d1d24]" />
               <h2 className="mb-5 text-2xl text-white font-medium group-hover:text-[#1d1d24]">Video & animatie</h2>
               <p className="text-[#717173] font-light text-lg group-hover:text-[#1d1d24]">I throw myself down among the tall grass by the stream as i lie close to the earth.</p>
             </div>
