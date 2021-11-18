48809014eb29189dd599165b0bf3b7ab0c20cf9c
Joel <joelmik123@gmail.com>
2021-11-18 16:52:02 +0100

    idk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,46 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
+        transparent: "transparent",
+        current: "currentColor",
         yellow: {
-          light: '#85d7ff',
-          DEFAULT: '#F1D302',
-          dark: '#009eeb',
+          light: "#85d7ff",
+          DEFAULT: "#F1D302",
+          dark: "#009eeb",
         },
         gray: {
-          darkest: '#1f2d3d',
-          dark: '#3c4858',
-          DEFAULT: '#101010',
-          light: '#1d1d24',
-          lightest: '#f9fafc',
-        }
+          darkest: "#1f2d3d",
+          dark: "#3c4858",
+          DEFAULT: "#101010",
+          light: "#1d1d24",
+          lightest: "#f9fafc",
+        },
       },
       outline: {
-        yellow: '2px solid #F1D302'
-      }
+        yellow: "2px solid #F1D302",
+      },
     },
     fontFamily: {
-      'staat': ['Staatliches', 'cursive'],
-      'poppins': ['Poppins'],
+      staat: ["Staatliches", "cursive"],
+      poppins: ["Poppins"],
     },
     backdropGrayscale: {
-      50: '.5'
+      50: ".5",
     },
     blur: {
-      'xs': '2px'
-    }
+      xs: "2px",
+    },
   },
   variants: {
     extend: {
       zIndex: {
-        '+10': '-10',
-       }
+        "+10": "-10",
+      },
     },
   },
   plugins: [],
-}
+};
