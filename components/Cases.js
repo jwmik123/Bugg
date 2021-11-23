dc11aaf7e36e9b5d2685d5fce454133f8afe113b
Joel <joelmik123@gmail.com>
2021-11-23 09:19:28 +0100

    cases commented

--- components/Cases.js
+++ components/Cases.js
@@ -4,30 +4,31 @@ import Image from "next/image";
 
 function Cases() {
   return (
-    <div className="w-full h-72 flex">
-      <div className="relative w-1/2 h-full flex">
-        <div className="absolute w-full h-full bg-gray-800 z-10 opacity-90 flex items-center justify-center">
-          <h1 className="text-white">Mursee</h1>
-        </div>
-        <Image src={mursee} layout="fill" objectFit="cover" />
-      </div>
-      <div className="group relative w-1/2 h-full">
-        <div className="absolute w-full h-full bg-gray-800 z-10 flex items-center opacity-50 justify-center cursor-pointer">
-          <Image
-            src={murseeLogo}
-            layout="fill"
-            objectFit="contain"
-            className="group-hover:scale-90"
-          />
-        </div>
-        <Image
-          src={mursee}
-          layout="fill"
-          objectFit="cover"
-          className="group-hover:scale-150"
-        />
-      </div>
-    </div>
+    // <div className="w-full h-72 flex">
+    //   <div className="relative w-1/2 h-full flex">
+    //     <div className="absolute w-full h-full flex items-center justify-center">
+    //       <Image src={mursee} layout="fill" objectFit="cover" />
+    //     </div>
+    //     <h1 className="text-white text-4xl z-10">Mursee</h1>
+    //   </div>
+    //   <div className="group relative w-1/2 h-full  transition-all ease-in-out duration-300">
+    //     <div className="absolute w-full h-full z-10 flex items-center justify-center cursor-pointer">
+    //       {/* <Image
+    //         src={murseeLogo}
+    //         layout="fill"
+    //         objectFit="contain"
+    //         className="group-hover:scale-90"
+    //       /> */}
+    //     </div>
+    //     <Image
+    //       src={mursee}
+    //       layout="fill"
+    //       objectFit="cover"
+    //       className="group-hover:scale-150"
+    //     />
+    //   </div>
+    // </div>
+    <div></div>
   );
 }
 
